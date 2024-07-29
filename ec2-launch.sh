d0d0493a81426d1689d0b6b7facccfebfde43f88
Kartikay <kartikayjoshiprivate@gmail.com>
2024-07-29 16:49:36 +0530

    Changes

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
-# 1. Create an spot instance
-# 2. Take the instance ip and register in DNS
+#1. Create an SPOT instance
+#2. Take that Instance IP & register in DNS
 
 if [ -z "$1" ]; then
   echo -e "\e[1;31mInput is missing\e[0m"
@@ -9,43 +9,42 @@ if [ -z "$1" ]; then
 fi
 
 COMPONENT=$1
+ENV=$2
 
-
-TEMP_ID="lt-060b9b29cc136a449"
-TEMP_VER=1
-ZONE_ID=Z03176063U3033ZBJYXA7
-
-CREATE_INSTANCE() {
-# Check if instance is already there if not create one
-
-aws ec2 describe-instance --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep - E 'running|stopped' &>/dev/null
-if [ $? -eq -0 ]; then
-  echo "\e[1;33mInstance is already there\e[0m"
-else
-  aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specification "ResourceType=instances,Tags=[{Key=Name,Value=${COMPONENT}}]"  "ResourceType=spot-instance-request,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+if [ ! -z "$ENV" ]; then
+  ENV="-${ENV}"
 fi
 
 
-sleep 10
+TEMP_ID="lt-0460a6c5d87a104d3"
+TEMP_VER=5
+ZONE_ID=Z06421191721I0AOBUGO2
 
-IPADDRESS=$(aws ec2 describe-instance --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIPAddress | sed 's/"//g' | grep -v  null)
+CREATE_INSTANCE() {
+  ## Check if instance is already there
+  aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+  if [ $? -eq -0 ]; then
+    echo -e "\e[1;33mInstance is already there\e[0m"
+  else
+    aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+  fi
 
-# Update the DNS record
+  sleep 10
+
+  IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
+
+  # Update the DNS record
   sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
-
-
 }
 
-if [ "$COMPONENT" == "all" ] ; then
-  for comp in frontend mongodb catalouge; do
-    COMPONENT=$comp
+if [ "$COMPONENT" == "all" ]; then
+  for comp in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment dispatch ; do
+    COMPONENT=$comp$ENV
     CREATE_INSTANCE
   done
 else
-   CREATE_INSTANCE
+  COMPONENT=$COMPONENT$ENV
+  CREATE_INSTANCE
 fi
-
-
-
 
